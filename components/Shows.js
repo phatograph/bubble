594149bbbc1cfdaf73645808fb89c9eca8d2d727
Pat Wangrungarun <phatograph@gmail.com>
2020-08-21 12:17:20 +0700

    Initial commit

---
+++ components/Shows.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import {get} from 'lodash'
+import className from 'classnames'
+
+const Shows = ({
+  data,
+}) => {
+  return (
+    <div>
+      {data.map((x, i) => (
+        <p key={i}>{get(x, 'show.name')}</p>
+      ))}
+    </div>
+  )
+}
+
+Shows.propTypes = {
+}
+
+export default Shows
---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import {get} from 'lodash'
+import className from 'classnames'
+import axios from 'axios'
+
+const Index = (props) => {
+  return (
+    <div className='Index'>
+    </div>
+  )
+}
+
+Index.propTypes = {}
+
+export default Index
