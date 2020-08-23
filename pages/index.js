abc236eaf06bdaeb8d35284d6b192200d12b3e84
Pat Wangrungarun <phatograph@gmail.com>
2020-08-23 18:20:57 +0700

    ___animate

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,8 @@ import * as d3 from 'd3'
 const Index = (props) => {
   const $svg = React.useRef()
   const $mainG = React.useRef()
+  const $$gs = React.useRef()
+  const $$circles = React.useRef()
 
   const radius = 20
 
@@ -16,7 +18,6 @@ const Index = (props) => {
     Array.from(Array(7)).map((x, i) => {
       if (i == 0) {
         return {
-          r: radius,
           fx: 800 / 2,
           fy: 400 / 2,
           comments: [
@@ -30,112 +31,16 @@ const Index = (props) => {
         }
       }
 
-      return {
-        r: radius,
-      }
+      return {}
     })
   )
+
   const ___simulation = React.useRef()
+  const ___animate = React.useRef()
 
   React.useEffect(() => {
     const $$svg = d3.select($svg.current)
     const $$mainG = d3.select($mainG.current)
-
-    ___simulation.current = d3
-      .forceSimulation(___nodes.current)
-      .force('charge', d3.forceManyBody().strength(5))
-      .force('center', d3.forceCenter(800 / 2, 400 / 2))
-      .force(
-        'collision',
-        d3.forceCollide().radius((d) => {
-          return d.r + 10
-        })
-      )
-      .on('tick', () => {
-        const $g = $$mainG
-          .selectAll('g.post')
-          .data(___nodes.current)
-          .join('g')
-          .classed('post', true)
-
-        $g.selectAll('circle')
-          .data((d) => {
-            return [
-              {
-                x: d.x,
-                y: d.y,
-              },
-            ]
-          })
-          .join('circle')
-          .attr('r', radius)
-          .attr('cx', (d) => d.x)
-          .attr('cy', (d) => d.y)
-
-        $g.selectAll('text.label')
-          .data((d, i) => {
-            return [
-              {
-                x: d.x,
-                y: d.y,
-                i,
-              },
-            ]
-          })
-          .join('text')
-          .classed('label', true)
-          .attr('x', (d) => d.x)
-          .attr('y', (d) => d.y)
-          .text((d) => d.i)
-          .on('click', (d) => {
-            ___nodes.current = ___nodes.current.map((x) => {
-              if (x.index == d.i) {
-                return {
-                  ...x,
-                  comments: [
-                    ...(get(x, 'comments') || []),
-                    {
-                      label: 'x',
-                    },
-                  ],
-                }
-              }
-
-              return x
-            })
-
-            ___simulation.current.nodes(___nodes.current)
-            ___simulation.current.alpha(1).restart()
-          })
-
-        $g.selectAll('g.comments')
-          .data((d, i) => {
-            if (!get(d, 'comments')) {
-              return []
-            }
-
-            return [
-              {
-                comments: (get(d, 'comments') || []).map((x, i) => {
-                  return {
-                    x: d.x,
-                    y: d.y,
-                    i,
-                    label: get(x, 'label'),
-                  }
-                }),
-              },
-            ]
-          })
-          .join('g')
-          .classed('comments', true)
-          .selectAll('text')
-          .data((d) => get(d, 'comments') || [])
-          .join('text')
-          .attr('x', (d) => d.x + Math.sin((d.i * Math.PI) / 6) * radius * 1.25)
-          .attr('y', (d) => d.y - Math.cos((d.i * Math.PI) / 6) * radius * 1.25)
-          .text((d) => d.label)
-      })
 
     $$svg.call(
       d3
@@ -153,6 +58,48 @@ const Index = (props) => {
           $$mainG.attr('transform', d3.event.transform)
         })
     )
+
+    $$svg.on('dblclick.zoom', null)
+
+    $$circles.current = $$mainG.selectAll('circle')
+
+    ___simulation.current = d3
+      .forceSimulation(___nodes.current)
+      .force('charge', d3.forceManyBody().strength(5))
+      .force('center', d3.forceCenter(800 / 2, 400 / 2))
+      .force(
+        'collision',
+        d3.forceCollide().radius((d) => {
+          return radius + 10
+        })
+      )
+      .on('tick', () => {
+        $$circles.current.attr('cx', (d) => d.x).attr('cy', (d) => d.y)
+      })
+      .stop()
+
+    ___animate.current = () => {
+      $$circles.current = $$circles.current.data(___nodes.current).join(
+        (enter) => {
+          return enter
+            .append('circle')
+            .attr('r', 5)
+            .call((enter) => {
+              return enter
+                .transition()
+                .duration(3000)
+                .attr('r', radius)
+            })
+        },
+        (update) => update,
+        (exit) => exit.remove()
+      )
+
+      ___simulation.current.nodes(___nodes.current)
+      ___simulation.current.alpha(1).restart()
+    }
+
+    ___animate.current()
   }, [])
 
   return (
@@ -182,19 +129,7 @@ const Index = (props) => {
             },
           ]
 
-          // ___nodes.current = ___nodes.current.map((x, i) => {
-          //   if (i == 0) {
-          //     return {
-          //       ...x,
-          //       r: 80,
-          //     }
-          //   }
-          //
-          //   return x
-          // })
-
-          ___simulation.current.nodes(___nodes.current)
-          ___simulation.current.alpha(1).restart()
+          ___animate.current()
         }}
       >
         Add node
